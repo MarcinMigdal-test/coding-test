db0e2f8c6a1ab8e163c337773ac659601e4292d6
MarcinMigdal-test <82544381+marcinmigdal-test@users.noreply.github.com>
2021-04-15 17:58:40 +0200

    Technology upgrade: Junit -> 5.x Spring -> 2.x Guava -> 30.x Awaitaility -> we will see, as there was groupId change

--- elevators/src/test/java/com/tingco/codechallenge/elevator/IntegrationTest.java
+++ elevators/src/test/java/com/tingco/codechallenge/elevator/IntegrationTest.java
@@ -1,20 +1,21 @@
 package com.tingco.codechallenge.elevator;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.tingco.codechallenge.elevator.config.ElevatorApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Boiler plate test class to get up and running with a test faster.
  *
  * @author Sven Wesley
- *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ElevatorApplication.class)
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ElevatorApplication.class})
+@WebAppConfiguration
 public class IntegrationTest {
 
     @Test
